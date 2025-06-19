ee7f32760b9b00c0565a3a0a6e2d95520c048619
lenz-B <154307607+lenz-B@users.noreply.github.com>
2025-06-19 13:25:17 +0400

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 "use client";
 
+import Search from "@/components/search";
 import { number } from "motion";
 import Image from "next/image";
 import { useState } from "react";
 
-const Button = ({text, count, setCount}) => {
-  const handleClick = () => {
-    if (text === '+') {
-      setCount(count + 1)
-    } else if (text === '-'){
-      setCount(count - 1)
-    } else {
-      setCount(0)
-    }
-  }
-
-  
-  return (
-    <button onClick={handleClick}>{text}</button>
-
-  );
-};
-
-
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
 
   return (
-    <div>
-      <h1>{count}</h1>
-     <Button text='-' count={count} setCount={setCount}/>
-     <Button text='+' count={count} setCount={setCount}/>
-     <Button text='reset' count={count} setCount={setCount}/>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      </div>
+    </main>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add `app` if you use the new Next.js App Router:
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
